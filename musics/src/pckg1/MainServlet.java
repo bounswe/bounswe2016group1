9ec582c56d26f4a591e9d081ea8fa1f759563dc0
Mavera07 <cyberoyal@windowslive.com>
2016-05-09 23:18:09 +0300

    12th

--- musics/src/pckg1/MainServlet.java
+++ musics/src/pckg1/MainServlet.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,21 +22,16 @@ import javax.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	static final String user = "root";
+	static final String pass = "12345678";
+	static final String url = "jdbc:mysql://localhost:3306/test";
 
-//  -----
-//	Connection conn = null;
-//	String url = "jdbc:mysql://localhost:3306/test";
-//	String user = "root";
-//	String pass = "12345678";
-//	try {
-//		conn = DriverManager.getConnection(url, user, pass);
-//		System.out.println("Yeap!");
-//	} catch (SQLException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	}   
+	static Connection conn = null;
+	static String query = null;
+	static ResultSet result = null;
+	static PreparedStatement statement = null;
 	
-	
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -42,13 +40,32 @@ public class MainServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		PrintWriter pa = response.getWriter(); 
-		pa.append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, pass);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		if(conn != null){
+			try {
+				query = "SELECT * FROM table1";
+				statement = conn.prepareStatement(query);
+				result = statement.executeQuery();
+				
+				while(result.next()){
+					System.out.println(result.getString(1)+" "+result.getString(2));
+				}
+				
+			} catch (SQLException e) {e.printStackTrace();}
+		}
 	}
 
 	/**
